471b22fd8a1cd6ac0338b142e05276ad0b3fa6c6
chloerihouay <chloerihouay@hotmail.com>
2020-08-13 16:03:03 +0200

    update cocktail mania

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "strawberry")
+# Ingredient.create(name: "gin")
+# Ingredient.create(name: "Lemon vodka")
+# Ingredient.create(name: "Dry Vermouth")
+# Ingredient.create(name: "Blended whiskey")
+# Ingredient.create(name: "tonic")
+# Ingredient.create(name: "coca")
+# Ingredient.create(name: "Strawberry schnapps")
+# Ingredient.create(name: "Triple sec")
+# Ingredient.create(name: "Orange bitters")
+# Ingredient.create(name: "Bourbon")
+# Ingredient.create(name: "Tequila")
+# Ingredient.create(name: "Añejo rum")
+# Ingredient.create(name: "Mango")
+
+Ingredient.destroy_all
+
+require 'json'
+require 'open-uri'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+ingredient['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
