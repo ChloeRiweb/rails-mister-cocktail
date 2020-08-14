65c4c75daa2e28b319f965adad0a1c070c6065bf
chloerihouay <chloerihouay@hotmail.com>
2020-08-14 12:14:59 +0200

    update for green rake

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,10 +10,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktail_path(@cocktail)
-    # raise
-    # @cocktail = Cocktail.new(params[:cocktail])
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
